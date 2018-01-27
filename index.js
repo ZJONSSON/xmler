39bb0dac4630969ff7589137a8f3ad3e26e5cd0b
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2018-01-27 15:30:42 -0500

    Move code to ES6

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
-var stream = require('stream'),
-    expat = require('node-expat');
+const stream = require('stream');
+const expat = require('node-expat');
 
 function coerce(e,fn) {
   var _value = e.toLowerCase();
-  if (typeof fn === 'function')
+  if (typeof fn === 'function') {
     return fn(e);
-  else if (!isNaN(e))
+  } else if (!isNaN(e)) {
     return +e;
-  else if (_value == 'true')
+  } else if (_value == 'true') {
     return true;
-  else if (_value == 'false')
+  } else if (_value == 'false') {
     return false;
-  else
+  } else {
     return e;
+  }
 }
 
 module.exports = function xmler(criteria,options) {
   options = options || {};
   criteria = criteria || 0;
   
-  var path = [],
-      depth = 0,
-      parser = new expat.Parser('UTF-8'),
-      transform = stream.Transform({objectMode:true}),
-      base,pointer,selected,current;
+  const parser = new expat.Parser('UTF-8');
+  const transform = stream.Transform({objectMode:true});
+  let path = [];
+  let depth = 0;
+  let base,pointer,selected,current;
 
   // Parse criteria into a function
-  criteria = [].concat(criteria).map(function(criteria) {
-    if (typeof criteria === 'function')
+  criteria = [].concat(criteria).map(criteria => {
+    if (typeof criteria === 'function') {
       return criteria;
-    else if (criteria instanceof RegExp)
+    } else if (criteria instanceof RegExp) {
       return d => criteria.exec(d.path.join(' '));
-    else if (!isNaN(criteria))
+    } else if (!isNaN(criteria)) {
       return d => d.depth == criteria;  
-    else
+    } else {
       return d => d.tag === criteria;
+    }
   });
 
   // The transform function feeds the expat parser
-  transform._transform = function(chunk,encoding,callback) {
+  transform._transform = (chunk,encoding,callback) => {
     parser.parse(chunk.toString());
     callback();
   };
 
   // For each new element we bump up depth and add to path
-  parser.on('startElement',function(tag,attr) {
+  parser.on('startElement', (tag,attr) => {
     path.push(tag);
     transform.emit('startElement', {tag, path: path.slice(), depth, attr});
     depth += 1;
   });
 
   // For each ending we take down one depth and pop from path
-  parser.on('endElement',function(tag) {
+  parser.on('endElement', tag => {
     depth -= 1;
     transform.emit('endElement',{tag, path: path.slice(), depth});
     path.pop();
   });
 
-  parser.on('text',function(text) {
-    transform.emit('text',text,path,depth);
-  });
+  parser.on('text', text => transform.emit('text',text,path,depth));
+  
+  parser.on('finish', () => transform.end());
 
-  parser.on('finish',function() {
-    transform.end();
-  });
-
-  transform.on('startElement',function(d) {
+  transform.on('startElement', d => {
     current = d;
     // If we already have a pointer, we need to create next depth and register current as parent
     if (pointer) {
-      var parent = pointer;
+      let parent = pointer;
       pointer = transform.pointer = {};
       Object.defineProperty(pointer,'parent',{value:parent, enumerable: false});
       
-      if (!parent[d.tag]) parent[d.tag] = options.arrayNotation ? [pointer] : pointer;
-      // If this child has already been registered, we ensure it's an array, and append to it
-      else {
+      if (!parent[d.tag]) {
+        parent[d.tag] = options.arrayNotation ? [pointer] : pointer;
+      } else {
+        // If this child has already been registered, we ensure it's an array, and append to it
         parent[d.tag] = [].concat(parent[d.tag]);
         parent[d.tag].push(pointer);
       }
@@ -86,7 +85,7 @@ module.exports = function xmler(criteria,options) {
     }
     if (pointer) {
       if (options.coerce || options.mergeAttr)
-        for (var key in d.attr) {
+        for (let key in d.attr) {
           if (options.coerce) 
             d.attr[key] = coerce(d.attr[key],options.coerce[key]);
           if (options.mergeAttr)
@@ -96,32 +95,43 @@ module.exports = function xmler(criteria,options) {
     }
   });
 
-  transform.on('text',function(e) {
+  transform.on('text',e => {
     e = e.toString().trim();
-    if (!pointer || !e.length) return;
-    if (options.coerce)
+    if (!pointer || !e.length) {
+      return;
+    }
+    if (options.coerce) {
       e = coerce(e,options.coerce[current.tag]);
+    }
     pointer.text = e;
   });
 
-  transform.on('endElement',function() {
+  transform.on('endElement', () => {
     // We ignore any elements when not within root structure
-    if (!pointer) return;
+    if (!pointer) {
+      return;
+    }
     
     if (depth === selected.depth) {
-      if (!options.textObject && Object.keys(base).length == 1 && base.text !== undefined)
+      if (!options.textObject && Object.keys(base).length == 1 && base.text !== undefined) {
         base = base.text;
+      }
       selected.value = base;
       transform.push(options.valuesOnly ? base : selected);
       base = pointer = null;
     } else {
-      var parent = pointer.parent;
+      const parent = pointer.parent;
       // On each closing we clean up if text is the only child
       if (!options.textObject && Object.keys(pointer).length == 1 && pointer.text !== undefined) {
-        if (!pointer.parent) pointer = pointer.text;
-        var p = pointer.parent[current.tag].length;
-        if (p) pointer.parent[current.tag][p-1] = pointer.text;
-        else pointer.parent[current.tag] = pointer.text;
+        if (!pointer.parent) {
+          pointer = pointer.text;
+        }
+        const p = pointer.parent[current.tag].length;
+        if (p) {
+          pointer.parent[current.tag][p-1] = pointer.text;
+        } else {
+          pointer.parent[current.tag] = pointer.text;
+        }
       }
       // get rid of the parent reference and move pointer to parent
       delete pointer.parent;
